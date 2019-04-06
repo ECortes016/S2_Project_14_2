20ac0be102b96bf4d560a16eef448f2614c984de
Emmanuel Cortes <ecortes016@west-mec.org>
2019-04-05 17:30:25 -0700

    Final Commit
    Ready for review. Updated the for loop since it wouldn't function correctly. Updated the foodID variable since it was storing the wrong information

--- sub_cart.js
+++ sub_cart.js
@@ -26,28 +26,41 @@
 window.onload = setupCart;
 
 function setupCart() {
+      // Stores a collection of elements belonging to the addButton class
       var addButtons = document.getElementsByClassName("addButton");
 
       for (var i = 0; i < addButtons.length; i++) {
+            // Every time the addButtons collection is clicked then the addItem function will be ran
             addButtons[i].onclick = addItem;
       }
 
       function addItem(e) {
+            // Stores the element next to the event that is being targeted
             var foodItem = e.target.nextElementSibling;
-
-            var foodID = document.getElementById("foodItem").value;
+            // Stores the id of the foodItem variable
+            var foodID = foodItem.getAttribute("id");
+            // Creates copies of the foodItem element and all of its descendants 
             var foodDescription = foodItem.cloneNode(true);
-
+            // Stores the element with the id of cart
             var cartBox = document.getElementById("cart");
-
             var duplicateOrder = false;
 
-            for (var i = 0; i < cartBox.length; i++) {
-                  if (i.cartBox) {
 
-                  } else {
-
+            for (var n = cartBox.firstChild; n = n.nextElementSibling; n !== null) {
+                  // If the id of the node is strictly equal to the foodID variable then the code within will run
+                  if (n.id === foodID) {
+                        duplicateOrder = true; 
+                        n.firstElementChild.textContent++;
+                        break;
                   }
+            }
+
+            // If the duplicateOrder is still false then the code within the if statement will run
+            if (duplicateOrder === false) {
+                  var orderCount = document.createElement("span");
+                  orderCount.textContent = "1";
+                  foodDescription.insertBefore(orderCount, foodDescription.firstChild);
+                  cartBox.appendChild(foodDescription);
             }
       }
 }
